e56b23152f4ae76750c8517e83838366f593ae80
Kushal J <jkushal208@gmail.com>
2025-10-10 17:09:52 +0530

    add sigup feature

--- BACKEND/src/server.js
+++ BACKEND/src/server.js
@@ -5,13 +5,14 @@ import dotenv from "dotenv/config"
 import authRoute from './routes/auth.route.js';
 import messagesRoute from './routes/message.route.js'
  import path from 'path';
+import connectDB from './lib/db.js'
 
 const PORT=process.env.PORT || 3000
 
 
-
 const app=express();
 
+app.use(express.json())
 const __dirname=path.resolve()
 
 
@@ -32,6 +33,7 @@ if(process.env.NODE_ENV=="production"){
 
 
 
-app.listen(PORT,(req,res)=>{
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT,async(req,res)=>{
+  console.log(`Server is running on port http://localhost:${PORT}`);
+  await connectDB()
 })

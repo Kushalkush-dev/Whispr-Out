c45427738ac914858e1f2db8ed638f289e23a0bb
Kushal J <jkushal208@gmail.com>
2025-10-14 13:22:54 +0530

    implemented rateLimit with Arcjet

--- BACKEND/src/routes/auth.route.js
+++ BACKEND/src/routes/auth.route.js
@@ -1,8 +1,16 @@
 import express from "express";
 import { signup , login, logout, profiepic } from "../controllers/auth.controller.js";
 import { protectRoutes } from "../middlewares/auth.middleware.js/protectRoutes.js";
+import { rateLimit } from "../middlewares/rateLimit.js";
 
 const router=express.Router()
+
+
+router.use(rateLimit)
+
+// router.get("/test",(req,res)=>{
+//   res.status(200).json({message:"This is Rate Limit test route"})
+// })
 
 router.post("/signup",signup)
 

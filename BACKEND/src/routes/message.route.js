3615b66d2f0e13522f17910cdb6f0ef90973aeb6
Kushal J <jkushal208@gmail.com>
2025-10-16 16:09:25 +0530

    add the get chatUsers api route and contollers

--- BACKEND/src/routes/message.route.js
+++ BACKEND/src/routes/message.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { getAllContacts, getMessages, sendMessage } from "../controllers/message.controller.js"
+import { chatPage, getAllContacts, getMessages, sendMessage } from "../controllers/message.controller.js"
 import { protectRoutes } from "../middlewares/auth.middleware.js/protectRoutes.js"
 
 const router=express.Router()
@@ -8,12 +8,9 @@ const router=express.Router()
 
 
 router.get('/contacts',protectRoutes,getAllContacts)
-
-
-router.get("/:id",protectRoutes,getMessages)
-
+router.get('/chat',protectRoutes,chatPage)
+router.get("/getmessages/:id",protectRoutes,getMessages)
 router.post("/send/:id",protectRoutes,sendMessage)
-
 
 // router.get('/send',(req,res)=>{
 //   res.send("This message send Route")

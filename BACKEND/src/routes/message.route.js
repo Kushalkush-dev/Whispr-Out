9b2a8506266773765291e0630e28047c79f0813a
Kushal J <jkushal208@gmail.com>
2025-10-16 16:19:09 +0530

    update the middleware usage structure in messageroutes.js

--- BACKEND/src/routes/message.route.js
+++ BACKEND/src/routes/message.route.js
@@ -1,24 +1,17 @@
 import express from "express"
 import { chatPage, getAllContacts, getMessages, sendMessage } from "../controllers/message.controller.js"
 import { protectRoutes } from "../middlewares/auth.middleware.js/protectRoutes.js"
+import { rateLimit } from "../middlewares/rateLimit.js"
 
 const router=express.Router()
 
 
+router.use(rateLimit,protectRoutes)
+
+router.get('/contacts',getAllContacts)
+router.get('/chat',chatPage)
+router.get("/getmessages/:id",getMessages)
+router.post("/send/:id",sendMessage)
 
 
-router.get('/contacts',protectRoutes,getAllContacts)
-router.get('/chat',protectRoutes,chatPage)
-router.get("/getmessages/:id",protectRoutes,getMessages)
-router.post("/send/:id",protectRoutes,sendMessage)
-
-// router.get('/send',(req,res)=>{
-//   res.send("This message send Route")
-// })
-
-
-
-// router.get('/receive',(req,res)=>{
-//   res.send("This message receive Route")
-// })
 export default router

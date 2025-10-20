3eaa8a4ea69e7b52df3ec20c8bbbe27430b1658a
Kushal J <jkushal208@gmail.com>
2025-10-20 19:23:31 +0530

    Confined ChatHeader in a sepearte componenet

--- FRONTEND/src/components/ChatContainer.jsx
+++ FRONTEND/src/components/ChatContainer.jsx
@@ -1,54 +1,23 @@
-import React, { useEffect } from 'react'
-import useChatStore from '../store/useChatStore'
-import { XIcon } from 'lucide-react'
+
+import ChatHeader from './ChatHeader'
 
 const ChatContainer = () => {
 
 
 
-const {selectedUser,setSelectedUser}= useChatStore()
 
+  return (
 
-useEffect(()=>{
-  const handleEscape=(event)=>{
-  
-    if(event.key==="Escape") setSelectedUser(null)
-  
-  }
+      <>
+      <ChatHeader/>
+      </>
 
-  window.addEventListener("keydown",handleEscape)
-
-  return()=> window.removeEventListener("keydown",handleEscape)
-},[setSelectedUser])
-
-
- return (
-
-  <div className='flex w-full px-3 py-2 items-center max-h-[65px] flex-1 justify-between border-b border-slate-600 '>
-
-    <div className='flex items-center gap-4'>
-
-      <div className='avatar online'>
-        <div className='rounded-full w-12 '>
-        <img src={selectedUser.profilepic || "/avatar.png"} alt={selectedUser.fullname} />
-        </div>
-      </div>
-
-
-      <div>
-      <h3 className='font-medium'>{selectedUser.fullname}</h3>
-      <p className='text-xs'>Online</p>
-      </div>
-
-    </div>
-
-    <div>
-      <button onClick={()=>setSelectedUser(null)} className='flex items-center hover:text-rose-500'><XIcon/></button>
-    </div>
-
-  </div>
-  
   )
+
+
+
+
+
 }
 
 export default ChatContainer

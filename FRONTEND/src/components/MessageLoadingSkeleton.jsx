d4bae3023ea30cb3f6f34d616765a560085e3092
Kushal J <jkushal208@gmail.com>
2025-10-20 21:16:56 +0530

    render the messages based on sender and receiver with loadskeleton and default new chat componenet

---
+++ FRONTEND/src/components/MessageLoadingSkeleton.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const MessageLoadingSkeleton = () => {
+  return (
+<div className="max-w-3xl mx-auto space-y-6">
+
+    {[1,2,3,4,5,6].map((ele)=>(
+
+      <div className={`chat ${ele%2!==0 ? "chat-start":"chat-end"} animate-pulse `}>
+        <div className="chat-bubble bg-slate-500/20 text-white w-20 md:w-40 "></div>
+      </div>
+
+    ))}
+
+</div>
+
+  );
+};
+
+export default MessageLoadingSkeleton;
